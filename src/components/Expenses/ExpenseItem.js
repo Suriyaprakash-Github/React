07278c7d717ee4f8f49819a103dc7137486a0e8c
Suriyaprakash-Github <yoursgump@gmail.com>
2022-12-27 23:06:20 +0530

    useState  and changing the value with state

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,21 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  const deleteHandler = () => {
-    console.log("Delete Handler");
+  const [title, setTitle] = useState(props.title);
+  const [amount, setCost] = useState(props.amount);
+  const renameHandler = () => {
+    setTitle("NOPE");
+    console.log({ title });
+  };
+  const costHandler = () => {
+    setCost(100);
+    console.log(amount);
   };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
       </div>
-      <button onClick={deleteHandler}>Delete Expense</button>
+      <button onClick={costHandler}>Cost Changer</button>
+      <button onClick={renameHandler}>Title Changer</button>
     </Card>
   );
 };
