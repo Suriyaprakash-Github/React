8decf992712a524a0817b7529e038ac83770e206
Suriyaprakash-Github <yoursgump@gmail.com>
2022-12-30 22:56:02 +0530

    Stateful Lists

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,29 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-  const [amount, setCost] = useState(props.amount);
-  const renameHandler = () => {
-    setTitle("NOPE");
-    console.log({ title });
-  };
-  const costHandler = () => {
-    setCost(100);
-    console.log(amount);
-  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
-        <div className="expense-item__price">${amount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={costHandler}>Cost Changer</button>
-      <button onClick={renameHandler}>Title Changer</button>
     </Card>
   );
 };
